5f9ca01f85c480bfe5aa259a1da8479862cd858b
streambinder <posta@davidepucci.it>
2020-01-14 14:41:15 +0100

    resource: add support for flushing assets

--- resource/parser.go
+++ resource/parser.go
@@ -1,7 +1,11 @@
 package resource
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
 
 	"gopkg.in/yaml.v2"
 )
@@ -23,4 +27,38 @@ func Parse(fname string) (*Asset, error) {
 
 	asset.Filename = fname
 	return asset, nil
+}
+
+// Flush updates the physical content of a given asset
+// with bumped fields
+func Flush(asset *Asset) error {
+	read, err := ioutil.ReadFile(asset.Filename)
+	if err != nil {
+		return err
+	}
+
+	var (
+		content    = string(read)
+		regVersion = regexp.MustCompile(`(?m)(^version\s+:)\s+` + regexp.QuoteMeta(asset.Version) + `$`)
+		regRelease = regexp.MustCompile(`(?m)(^release\s+:)\s+` + regexp.QuoteMeta(strconv.Itoa(asset.Release)) + `$`)
+	)
+	content = regVersion.ReplaceAllString(content, `$1 `+asset.BumpVersion)
+	content = regRelease.ReplaceAllString(content, `$1 `+strconv.Itoa(asset.BumpRelease))
+	for entry := range asset.Source {
+		for source, hash := range asset.Source[entry] {
+			for bumpSource, bumpHash := range asset.BumpSource[entry] {
+				fmt.Println(regexp.QuoteMeta(source), regexp.QuoteMeta(hash))
+				fmt.Println(bumpSource, bumpHash)
+				regSource := regexp.MustCompile(`(\s{4}\-)\s+` + regexp.QuoteMeta(source) + `\s+:\s+` + regexp.QuoteMeta(hash))
+				content = regSource.ReplaceAllString(content, `$1 `+bumpSource+` : `+bumpHash)
+			}
+		}
+	}
+
+	err = ioutil.WriteFile(asset.Filename, []byte(content), 0)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }

680ecc748cdf2f5c465876ab02133af60f1f4593
streambinder <posta@davidepucci.it>
2020-01-15 16:51:44 +0100

    config: add support for gitlab token

--- config/config.go
+++ config/config.go
@@ -3,11 +3,10 @@ package config
 // Config represents the abstraction of the parsed
 // configuration file
 type Config struct {
-	Github *Github `yaml:"github"`
-}
-
-// Github represents the configuration section
-// related to the Github provider
-type Github struct {
-	API string `yaml:"api"`
+	Github struct {
+		API string `yaml:"api"`
+	} `yaml:"github"`
+	Gitlab struct {
+		API string `yaml:"api"`
+	} `yaml:"gitlab"`
 }

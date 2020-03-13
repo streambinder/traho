250dbcef33d8ef68bb05f3599d3fc668a5bbc9f4
streambinder <posta@davidepucci.it>
2020-03-13 23:14:38 +0100

    resource: version: more strict regexes

--- resource/version.go
+++ resource/version.go
@@ -1,13 +1,26 @@
 package resource
 
-import "regexp"
+import (
+	"regexp"
+)
 
 var (
-	regVersion = regexp.MustCompile("[^0-9\\.]+")
+	regVersions = []*regexp.Regexp{
+		regexp.MustCompile(`(?m)([0-9]{1}[0-9\.]+[0-9a-zA-Z\-]+)`),
+		regexp.MustCompile(`(?m)^v([0-9]{1}[0-9\.]+$)`),
+	}
 )
 
 // StripVersion returns the normalized variant string
 // corresponding to the given version string
 func StripVersion(version string) string {
-	return regVersion.ReplaceAllString(version, "")
+	for _, reg := range regVersions {
+		for _, matches := range reg.FindAllStringSubmatch(version, -1) {
+			if len(matches) > 1 {
+				return matches[1]
+			}
+		}
+	}
+
+	return version
 }

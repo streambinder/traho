f5d955ecd3b53aa34ee5de2751da5aefeeccfb58
streambinder <posta@davidepucci.it>
2020-04-21 12:36:26 +0200

    provider: fileserver: add unit tests

---
+++ provider/fileserver/fixedPath_test.go
@@ -0,0 +1,26 @@
+package fileserver
+
+import (
+	"testing"
+)
+
+func TestParseAddress(t *testing.T) {
+	var (
+		base         = "https://davidepucci.it/solbump"
+		resource     = "1.0.0.tar.bz2"
+		url          = base + "/" + resource
+		address, err = parseAddress(url)
+	)
+
+	if err != nil {
+		t.Errorf("Unexpected failure while parsing %s", url)
+	}
+
+	if address.Base != base {
+		t.Errorf("Unexpected base mismatch: expected %s, got %s", base, address.Base)
+	}
+
+	if address.Resource != resource {
+		t.Errorf("Unexpected resource mismatch: expected %s, got %s", resource, address.Resource)
+	}
+}
---
+++ provider/fileserver/variablePath_test.go
@@ -0,0 +1,31 @@
+package fileserver
+
+import (
+	"testing"
+)
+
+func TestParseVariableAddress(t *testing.T) {
+	var (
+		base         = "https://davidepucci.it/solbump"
+		release      = "1.0.0"
+		resource     = "1.0.0.tar.bz2"
+		url          = base + "/" + release + "/" + resource
+		address, err = parseVariableAddress(url)
+	)
+
+	if err != nil {
+		t.Errorf("Unexpected failure while parsing %s", url)
+	}
+
+	if address.Base != base {
+		t.Errorf("Unexpected base mismatch: expected %s, got %s", base, address.Base)
+	}
+
+	if address.Release != release {
+		t.Errorf("Unexpected release mismatch: expected %s, got %s", release, address.Release)
+	}
+
+	if address.Resource != resource {
+		t.Errorf("Unexpected resource mismatch: expected %s, got %s", resource, address.Resource)
+	}
+}

9043acbe7ecb7844ef5fde5e85cd3a83e3b4f564
streambinder <posta@davidepucci.it>
2020-04-21 12:17:51 +0200

    provider: add unit tests

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -9,27 +9,33 @@ import (
 	"github.com/streambinder/solbump/provider/undated"
 )
 
+type source struct {
+	url      string
+	version  string
+	provider string
+}
+
 const version = "1.0.0"
 
-var sources = map[string]string{
-	"git|https://github.com/streambinder/solbump.git":                                                  new(github.TagProvider).Name(),
-	"https://github.com/streambinder/solbump/releases/download/" + version + "/asset.zip":              new(github.AssetProvider).Name(),
-	"https://github.com/streambinder/solbump/archive/" + version + ".tar.gz":                           new(github.TarballProvider).Name(),
-	"https://gitlab.com/streambinder/solbump/-/archive/" + version + "/solbump-" + version + ".tar.gz": new(gitlab.Provider).Name(),
-	"https://davidepucci.it/solbump/" + version + "/solbump-" + version + ".tar.bz2":                   new(fileserver.VariablePathProvider).Name(),
-	"https://davidepucci.it/solbump/solbump-" + version + ".tar.bz2":                                   new(fileserver.FixedPathProvider).Name(),
-	"https://davidepucci.it/download?project=Solbump":                                                  new(undated.Provider).Name(),
+var sources = []source{
+	{"git|https://github.com/streambinder/solbump.git", version, new(github.TagProvider).Name()},
+	{"https://github.com/streambinder/solbump/releases/download/" + version + "/asset.zip", version, new(github.AssetProvider).Name()},
+	{"https://github.com/streambinder/solbump/archive/" + version + ".tar.gz", version, new(github.TarballProvider).Name()},
+	{"https://gitlab.com/streambinder/solbump/-/archive/" + version + "/solbump-" + version + ".tar.gz", version, new(gitlab.Provider).Name()},
+	{"https://davidepucci.it/solbump/" + version + "/solbump-" + version + ".tar.bz2", version, new(fileserver.VariablePathProvider).Name()},
+	{"https://davidepucci.it/solbump/solbump-" + version + ".tar.bz2", version, new(fileserver.FixedPathProvider).Name()},
+	{"https://davidepucci.it/download?project=Solbump", "20060102", new(undated.Provider).Name()},
 }
 
 func TestFor(t *testing.T) {
-	for key, val := range sources {
-		prov, err := For(key, version)
+	for _, source := range sources {
+		prov, err := For(source.url, source.version)
 		if err != nil {
-			t.Errorf("Unexpected failure while getting provider for %s: %s", key, err)
+			t.Errorf("Unexpected failure while getting provider for %s, version, %s", source.url, err)
 		}
 
-		if prov.Name() != val {
-			t.Errorf("Unexpected %s provider: expected %s, got %s", key, val, prov.Name())
+		if prov.Name() != source.provider {
+			t.Errorf("Unexpected %s provider, version, expected %s, got %s", source.url, source.provider, prov.Name())
 		}
 	}
 }

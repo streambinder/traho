885d4f1dfbde8d78d3ed2aea54dd9b30beb47944
streambinder <posta@davidepucci.it>
2021-01-04 13:18:48 +0100

    config: add custom gitlab domains and tokens

--- config/config.go
+++ config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"strings"
+)
+
 // Config represents the abstraction of the parsed
 // configuration file
 type Config struct {
@@ -7,7 +11,11 @@ type Config struct {
 		API string `yaml:"api"`
 	} `yaml:"github"`
 	Gitlab struct {
-		API string `yaml:"api"`
+		API     string `yaml:"api"`
+		Domains []struct {
+			Name string `yaml:"name"`
+			API  string `yaml:"api"`
+		} `yaml:"domains"`
 	} `yaml:"gitlab"`
 }
 
@@ -16,4 +24,16 @@ var config *Config
 // Get current config instance
 func Get() *Config {
 	return config
+}
+
+// HasGitlabDomain returns true if config has the given
+// domain defined as custom Gitlab domain
+func HasGitlabDomain(domain string) bool {
+	domain = strings.ToLower(domain)
+	for _, customDomain := range config.Gitlab.Domains {
+		if strings.ToLower(customDomain.Name) == domain {
+			return true
+		}
+	}
+	return false
 }
